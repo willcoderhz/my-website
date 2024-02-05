9e93a87f73845e810d99efb9c18d1591b490f016
willyao666 <13605529+willyao666@user.noreply.gitee.com>
2024-02-05 19:10:41 +0800

    link

--- src/components/WelcomePage/index.js
+++ src/components/WelcomePage/index.js
@@ -13,7 +13,7 @@ function WelcomePage() {
         <p>Passionate Frontend Engineer.</p>
         <p>Driven by Design and Coding.</p>
         <p>Tech Stack : HTML, CSS, JavaScript, React, Node.js, SQL</p>
-        <a href="https://drive.google.com/file/d/1Xm7VVRC75Qo6hYHwmvU9t4QEsr9fyqQm/view?usp=drive_link" className="resume-button">Resume</a>
+        <a href="https://drive.google.com/file/d/1xHvMNSgwEc-4jqkdVj8Lk9rvaR6SfjLj/view?usp=sharing" className="resume-button">Resume</a>
         <a href="mailto:paulyao22@gmail.com" className="resume-button">E-Mail</a>
         <div className="social-icons">
         <a href="https://www.linkedin.com/in/will-yao-0a8a41271/" target="_blank" rel="noopener noreferrer">
